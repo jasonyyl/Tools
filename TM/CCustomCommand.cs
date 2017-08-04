ea3984e6aeeec0ec7507365288bbb60e74f6d9d9
jason_yyl <vilskyan@gmail.com>
2017-08-05 01:10:48 +0800

    create

---
+++ TM/CCustomCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TM
+{
+    public static class CCustomCommand
+    {
+        public static RoutedCommand ExportTable = new RoutedCommand();
+        public static RoutedCommand ExportTableBinary = new RoutedCommand();
+    }
+}
---
+++ TM/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TM
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_ExportTable_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Table");
+        }
+        private void Command_ExportTable(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Table");
+        }
+        private void MenuItem_ExportTableBinary_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TableBinary");
+        }
+        private void Command_ExportTableBinary(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("TableBinary");
+        }
+    }
+}
